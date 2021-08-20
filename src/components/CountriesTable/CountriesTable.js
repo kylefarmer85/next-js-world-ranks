c2c1c7ad4aa22c64a3dac6ae3bd857712820bdf7
kylefarmer85 <kylefarmer85@gmail.com>
2021-08-19 21:11:58 -0500

    create orderBy func for countries, direction

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,6 +1,21 @@
 import styles from './CountriesTable.module.css';
 
+const orderBy = (countries, direction) => {
+  if (direction === 'asc') {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? 1 : -1
+    );
+  }
+  if (direction === 'desc') {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? -1 : 1
+    );
+  }
+};
+
 const CountriesTable = ({ countries }) => {
+  const orderedCountries = orderBy(countries, 'desc');
+
   return (
     <div>
       <div className={styles.heading}>
@@ -13,7 +28,7 @@ const CountriesTable = ({ countries }) => {
         </button>
       </div>
 
-      {countries.map(country => (
+      {orderedCountries.map(country => (
         <div className={styles.row}>
           <div className={styles.name}>{country.name}</div>
           <div className={styles.population}>{country.population}</div>
